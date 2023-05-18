afad2f7f61637559093b20ab8bf9965f55fc2dee
Hugo Findlay <2554911f@student.gla.ac.uk>
2023-05-18 18:50:06 +0100

    completed 3.2

--- hello.hs
+++ hello.hs
@@ -1,10 +1,27 @@
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
 
 makeHtml title body = html_ ( head_ ( title_ title) <> (body_ body))
 
-myhtml = makeHtml "Mitski" "A burning hill"
+myhtml = makeHtml "Mitski" (h1_ "Laurel Hell" <> p_ "A burning hill")
 
 main = putStrLn myhtml
