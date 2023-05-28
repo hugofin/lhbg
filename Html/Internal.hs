76e4ffd7bf30a4ebdf33a65e3b5d94a67025752d
Hugo Findlay <2554911f@student.gla.ac.uk>
2023-05-28 11:43:50 +0100

    section 3 done

--- Html/Internal.hs
+++ Html/Internal.hs
@@ -1,23 +1,35 @@
 module Html.Internal where
 
 newtype Html = Html String
+
 newtype Structure = Structure String
 
 type Title = String
 
 html_ :: Title -> Structure -> Html
-html_ title content = Html 
-  (el "html" 
-    (el "head" (el "title" (escape title))
-      <> el "body" (getStructureString content)
+html_ title content =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" (escape title))
+            <> el "body" (getStructureString content)
+        )
     )
-  )
 
 p_ :: String -> Structure
 p_ = Structure . el "p" . escape
 
 h1_ :: String -> Structure
 h1_ = Structure . el "h1" . escape
+
+ul_ :: [Structure] -> Structure
+ul_ = Structure . el "ul" . concatMap (el "li" . getStructureString)
+
+ol_ :: [Structure] -> Structure
+ol_ = Structure . el "ol" . concatMap (el "li" . getStructureString)
+
+code_ :: String -> Structure
+code_ = Structure . el "pre" . escape
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
@@ -33,14 +45,12 @@ render html = case html of Html str -> str
 
 escape :: String -> String
 escape =
-  let
-    escapeChar c =
-      case c of
-        '<' -> "&lt;"
-        '>' -> "&gt;"
-        '&' -> "&amp;"
-        '"' -> "&quot;"
-        '\'' -> "&#39;"
-        _ -> [c]
-  in
-    concat . map escapeChar
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot;"
+          '\'' -> "&#39;"
+          _ -> [c]
+   in concatMap escapeChar
